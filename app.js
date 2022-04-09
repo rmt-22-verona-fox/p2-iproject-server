403379bcb84313fac103d6e88bee374fbe79307c
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-04-10 00:04:56 +0700

    added another app initialization to the battlefield!

---
+++ app.js
@@ -0,0 +1,17 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Development server is up! on ${port}`);
+});
