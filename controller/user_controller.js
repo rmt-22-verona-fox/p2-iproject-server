3c21626d9832012605c73d5b016af96b88fa577a
fitrah ananda <fitrahnnd56@gmail.com>
2022-04-19 17:17:02 +0700

    add login feature

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,6 +1,5 @@
 const { User } = require("../models");
 
-
 class userController {
   static async register(req, res, next) {
     try {
@@ -15,6 +14,41 @@ class userController {
   }
 
   
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw { name: "Email is required" };
+      }
+
+      if (!password) {
+        throw { name: "Password is required" };
+      }
+
+      const Users = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!Users) {
+        throw { name: "Invalid email/password" };
+      }
+
+      if (!bcrypt.compareSync(password, Users.password)) {
+        throw { name: "Invalid email/password" };
+      }
+
+      const token = tokenCreate({
+        id: Users.id,
+        email: Users.email,
+      });
+      res.status(200).json({
+        access_token: token,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = userController;
