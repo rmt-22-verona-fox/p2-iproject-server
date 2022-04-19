131dfc326e24b017b21bbfa70aa94c1483770b9d
KevinTan19 <kepintan9967@gmail.com>
2022-04-19 19:56:57 +0700

    feature-fetch-brands

--- db/products.js
+++ db/products.js
@@ -1,17 +1,7 @@
 const axios = require("axios");
 
-const options = {
-    method: 'GET',
-    url: 'https://the-sneaker-database.p.rapidapi.com/sneakers',
-    params: { limit: '100' },
-    headers: {
-        'X-RapidAPI-Host': 'the-sneaker-database.p.rapidapi.com',
-        'X-RapidAPI-Key': process.env.X_RapidAPI_Key
-    }
-};
 
-
-function getSneakers() {
+function getSneakers(options) {
     return axios.request(options)
     let total = response.data.count
     let listSneakers = response.data.results
