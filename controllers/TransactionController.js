9e508e4fbcade0bc4667591d04dd1415ab30059d
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-04-19 16:45:38 +0700

    Tes push branch

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -53,15 +53,143 @@ class TransactionController {
   /* NEED FRONT END INTEGRATION */
   static async payment(req, res, next) {
     try {
+      // res.header("Authorization", process.env.MIDTRANS_SECRET);
+
       let parameter = {
         transaction_details: {
-          order_id: "test-transaction-123",
-          gross_amount: 200000,
-        },
-        credit_card: {
-          secure: true,
+          order_id: "123",
+          gross_amount: 10000,
         },
       };
+
+      // let parameter = {
+      //   transaction_details: {
+      //     order_id: "HALO",
+      //     gross_amount: 10000,
+      //   },
+      //   item_details: [
+      //     {
+      //       id: "ITEM1",
+      //       price: 10000,
+      //       quantity: 1,
+      //       name: "Midtrans Bear",
+      //       brand: "Midtrans",
+      //       category: "Toys",
+      //       merchant_name: "Midtrans",
+      //     },
+      //   ],
+      //   customer_details: {
+      //     first_name: "TEST",
+      //     last_name: "MIDTRANSER",
+      //     email: "test@midtrans.com",
+      //     phone: "+628123456",
+      //     billing_address: {
+      //       first_name: "TEST",
+      //       last_name: "MIDTRANSER",
+      //       email: "test@midtrans.com",
+      //       phone: "081 2233 44-55",
+      //       address: "Sudirman",
+      //       city: "Jakarta",
+      //       postal_code: "12190",
+      //       country_code: "IDN",
+      //     },
+      //     shipping_address: {
+      //       first_name: "TEST",
+      //       last_name: "MIDTRANSER",
+      //       email: "test@midtrans.com",
+      //       phone: "0 8128-75 7-9338",
+      //       address: "Sudirman",
+      //       city: "Jakarta",
+      //       postal_code: "12190",
+      //       country_code: "IDN",
+      //     },
+      //   },
+      //   enabled_payments: [
+      //     "credit_card",
+      //     "cimb_clicks",
+      //     "bca_klikbca",
+      //     "bca_klikpay",
+      //     "bri_epay",
+      //     "echannel",
+      //     "permata_va",
+      //     "bca_va",
+      //     "bni_va",
+      //     "bri_va",
+      //     "other_va",
+      //     "gopay",
+      //     "indomaret",
+      //     "danamon_online",
+      //     "akulaku",
+      //     "shopeepay",
+      //   ],
+      //   credit_card: {
+      //     secure: true,
+      //     channel: "migs",
+      //     bank: "bca",
+      //     installment: {
+      //       required: false,
+      //       terms: {
+      //         bni: [3, 6, 12],
+      //         mandiri: [3, 6, 12],
+      //         cimb: [3],
+      //         bca: [3, 6, 12],
+      //         offline: [6, 12],
+      //       },
+      //     },
+      //     whitelist_bins: ["48111111", "41111111"],
+      //     dynamic_descriptor: {
+      //       merchant_name: "Fuji Apple Inc",
+      //       city_name: "Jakarta",
+      //       country_code: "ID",
+      //     },
+      //   },
+      //   bca_va: {
+      //     va_number: "12345678911",
+      //     sub_company_code: "00000",
+      //     free_text: {
+      //       inquiry: [
+      //         {
+      //           en: "text in English",
+      //           id: "text in Bahasa Indonesia",
+      //         },
+      //       ],
+      //       payment: [
+      //         {
+      //           en: "text in English",
+      //           id: "text in Bahasa Indonesia",
+      //         },
+      //       ],
+      //     },
+      //   },
+      //   bni_va: {
+      //     va_number: "12345678",
+      //   },
+      //   bri_va: {
+      //     va_number: "1234567891234",
+      //   },
+      //   permata_va: {
+      //     va_number: "1234567890",
+      //     recipient_name: "SUDARSONO",
+      //   },
+      //   shopeepay: {
+      //     callback_url: "http://shopeepay.com",
+      //   },
+      //   gopay: {
+      //     enable_callback: true,
+      //     callback_url: "http://gopay.com",
+      //   },
+      //   callbacks: {
+      //     finish: "https://demo.midtrans.com",
+      //   },
+      //   expiry: {
+      //     start_time: "2025-12-13 18:11:08 +0700",
+      //     unit: "minutes",
+      //     duration: 1,
+      //   },
+      //   custom_field1: "custom field 1 content",
+      //   custom_field2: "custom field 2 content",
+      //   custom_field3: "custom field 3 content",
+      // };
 
       const transaction = await snap.createTransaction(parameter);
 
@@ -71,6 +199,10 @@ class TransactionController {
       let transactionRedirectUrl = transaction.redirect_url;
 
       console.log("transactionRedirectUrl:", transactionRedirectUrl);
+      res.status(200).json({
+        token: transactionToken,
+        redirect_url: transactionRedirectUrl,
+      });
     } catch (err) {
       console.log("Error occured:", err.message);
     }
