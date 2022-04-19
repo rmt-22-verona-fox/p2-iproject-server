72d2c19eea3b188e7e34d7fdf6eb0269d98fcbb4
fitrah ananda <fitrahnnd56@gmail.com>
2022-04-19 17:11:44 +0700

    add register feature

---
+++ app.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
