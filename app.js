15f11c4bd78566ae795a9d7b5116a7f119148c45
fitrah ananda <fitrahnnd56@gmail.com>
2022-04-19 15:49:44 +0700

    add login feature

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const router = require("./router/index");
+const error = require("./middleware/errorHandler");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", router);
+app.use(error);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
