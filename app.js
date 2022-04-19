25bfb750bb7cbd906b68472fafdd7d7dbc44536a
KevinTan19 <kepintan9967@gmail.com>
2022-04-19 16:20:17 +0700

    develop & login

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const getBrands = require('./db/brands')
 const getSneakers = require('./db/products')
