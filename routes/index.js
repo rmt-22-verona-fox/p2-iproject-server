9863febc310efeb2bf1ed629d3767635cbf772ed
rickyDefianS <rickydefians@gmail.com>
2022-04-20 08:46:13 +0700

    - Setup route for authentication user - Setup middleware for authentication user

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const routerRegister = require("./register");
 const routerLogin = require("./login");
+const { authenticationUser } = require("../middlewares/authentication")
 
 router.use("/register", routerRegister);
 router.use("/login", routerLogin);
+router.use(authenticationUser)
 
 module.exports = router;
