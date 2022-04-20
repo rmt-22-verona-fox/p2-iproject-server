313e8d4ca4befdb27321e6b91b19897ed3555bcf
Romzi Fauzi <romziflubis@gmail.com>
2022-04-20 13:16:29 +0700

    add new migration to add new column in users table,then run the migrate

---
+++ migrations/20220420060256-add-column-role-and-seller-to-tabel-user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users', 'role', Sequelize.STRING);
+    await queryInterface.addColumn('Users', 'seller', Sequelize.BOOLEAN);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'role');
+    await queryInterface.removeColumn('Users', 'seller');
+  },
+};
