1e7b584664f2da7176ab4a4d8952d2c87f1b6012
adlanmsyariaty <adlanmsyariaty@gmail.com>
2022-04-21 06:50:05 +0700

    Break errorhandlers to middlewares folder

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const Controller = require('./controllers/controller')
 const authentication = require('./middlewares/authentication')
+const errorHandlers = require('./middlewares/errorHandlers')
 
 app.use(cors())
 app.use(express.json())
@@ -18,55 +19,7 @@ app.use(authentication)
 app.post('/booking', Controller.bookingHotel)
 app.get('/customers', Controller.getTicket)
 
-app.use((err, required, res, next) => {
-    console.log(err)
-    if (err.name === 'SequelizeValidationError') {
-        err = err.errors.map(el => el.message)
-        res.status(400).json({
-            message: err
-        })
-    } else if (err.name === 'SequelizeUniqueConstraintError') {
-        res.status(400).json({
-            message: 'Your email is already registered'
-        })
-    } else if (err.name === 'EMAIL_PASSWORD_IS_REQUIRED') {
-        res.status(400).json({
-            message: 'Email & password is required'
-        })
-    } else if (err.name === 'EMAIL_IS_REQUIRED') {
-        res.status(400).json({
-            message: 'Email is required'
-        })
-    } else if (err.name === 'PASSWORD_IS_REQUIRED') {
-        res.status(400).json({
-            message: 'Password is required'
-        })
-    } else if (err.name === 'ALREADY_BOOKED') {
-        res.status(400).json({
-            message: 'You are already booked the ticket'
-        })
-    } else if (err.name === 'USER_NOT_FOUND') {
-        res.status(401).json({
-            message: 'Invalid email or password'
-        })
-    } else if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
-        res.status(401).json({
-            message: 'Invalid identification token, please login first'
-        })
-    } else if (err.name === 'Unauthorized') {
-        res.status(401).json({
-            message: 'Unauthorized access account'
-        })
-    } else if (err.name === 'Forbidden') {
-        res.status(403).json({
-            message: 'Forbidden access'
-        })
-    } else {
-        res.status(500).json({
-            message: 'Internal Server Error'
-        })
-    }
-})
+app.use(errorHandlers)
 
 app.listen(port, () => {
   console.log(`This app listening on port ${port}`)
