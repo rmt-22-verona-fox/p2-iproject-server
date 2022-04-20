40dbb70e73a9233924dd3f22ce4a87ff4e403b8d
WibuSyariah <mspa43@gmail.com>
2022-04-21 00:33:19 +0700

    POST /customers/login added

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -26,6 +26,41 @@ class customerController {
       next(error);
     }
   }
+
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const customer = await Customer.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!customer) {
+        throw { name: "INVALID_EMAIL" };
+      }
+
+      const isMatch = comparePassword(password, customer.password);
+
+      if (!isMatch) {
+        throw { name: "INVALID_PASSWORD" };
+      }
+
+      const payload = { CustomerId: customer.id, email: customer.email };
+
+      const token = payloadToToken(payload);
+
+      res.status(200).json({
+        statusCode: 200,
+        message: "Customer logged in successfully",
+        access_token: token,
+        CustomerId: customer.id,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = customerController;
--- routes/customer.js
+++ routes/customer.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const customerController = require("../controllers/customerCOntroller");
 
 router.post("/register", customerController.register);
+router.post("/login", customerController.login);
 
 module.exports = router;
