03ec3ed2834ad6347393c99baa10b21bfab7ea05
adlanmsyariaty <adlanmsyariaty@gmail.com>
2022-04-20 20:50:14 +0700

    succeed to book hotel from customer using create method from sequelize

--- models/ticket.js
+++ models/ticket.js
@@ -11,19 +11,46 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Ticket.hasOne(models.Customer)
+      Ticket.belongsTo(models.Customer, {foreignKey: 'CustomerId'})
     }
   }
   Ticket.init({
-    hotel: DataTypes.STRING,
-    lat: DataTypes.INTEGER,
-    lng: DataTypes.INTEGER,
-    price: DataTypes.INTEGER,
-    roomType: DataTypes.STRING,
-    class: DataTypes.INTEGER,
-    CustomerId: DataTypes.INTEGER,
-    checkIn: DataTypes.DATE,
-    checkOut: DataTypes.DATE
+    hotel: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    lng: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    roomType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    hotelClass: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    CustomerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    checkIn: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    checkOut: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Ticket',
