14585429b60e622220a037959a51ae0c36395a5e
WibuSyariah <mspa43@gmail.com>
2022-04-22 06:52:31 +0700

    authentication added to PATCH /events/patch-hands/:EventId

--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const eventController = require("../controllers/eventController");
+const authentication = require("../middlewares/authentication");
 
 router.get("/home-page", eventController.homePage);
 router.get("/list", eventController.list);
-router.patch("/patch-hands/:EventId", eventController.patchHands);
+router.patch(
+  "/patch-hands/:EventId",
+  authentication,
+  eventController.patchHands
+);
 
 module.exports = router;
