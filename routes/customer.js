4ee8330a20dfb80ef7d0edd770e474573c89bd8d
WibuSyariah <mspa43@gmail.com>
2022-04-22 06:48:23 +0700

    minor typo removed

--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const customerController = require("../controllers/customerCOntroller");
+const customerController = require("../controllers/customerController");
 const authentication = require("../middlewares/authentication");
 
 router.post("/register", customerController.register);
